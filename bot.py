501535feb39ae89a2aa876f53464f2322d53e0f8
Darrin Knorr <knorr.darrin@gmail.com>
2020-09-08 09:20:20 -0500

    setup discord bot

---
+++ bot.py
@@ -0,0 +1,13 @@
+import discord
+
+client = discord.Client()
+token = 'NjgzMzE3MTM3NTc4MDAwNDQ1.XlpyrQ.pzJ5GXsNgrueLbGnlP_yTsOGR-E'
+
+@client.event
+async def on_ready():
+    print("we are ready!")
+
+@client.event
+async def 
+
+client.run(token)
