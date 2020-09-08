251ce212f9208c592fa7581b1db701ff7132e44e
Darrin Knorr <knorr.darrin@gmail.com>
2020-09-08 12:52:09 -0500

    updated the commands

--- bot.py
+++ bot.py
@@ -1,13 +1,29 @@
 import discord
+from discord.ext import commands
 
-client = discord.Client()
+bot = discord.Client()
+client = commands.Bot(command_prefix='?')
+serverID = '752885344076169340'
+
+@client.command()
+async def queue(ctx):
+    await ctx.send(f"""channel: {ctx.channel}""")
+    await ctx.send(f"""Guild: {ctx.guild}""")
+    await ctx.send(f"""Author: {ctx.author}""")
 
 @client.event
 async def on_ready():
     print("we are ready!")
 
 # @client.event
-# async def 
+# async def on_message(message):
+#     if message.author == client.user:
+#         return
+#     if message.content.startswith('$hello'):
+#         await message.channel.send('Hello!')
+#     elif message.content == 'helpme':
+#         await message.channel.send('heling now bud')
+
 
 token = input("Enter Token: ")
 client.run(token)
