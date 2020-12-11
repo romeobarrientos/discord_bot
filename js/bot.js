55a2d0da26701df2ecde044b27bee784b578d386
knorrcodes <47395767+knorrcodes@users.noreply.github.com>
2020-12-10 18:22:02 -0600

    added tiny penis command

--- js/bot.js
+++ js/bot.js
@@ -111,11 +111,32 @@ client.on('message', msg => {
 
 //mute channel command
     else if (command === 'mc') {
-        mems = msg.member.voice.channel.members;
-        mems.forEach(x => {
-            console.log(x.id)
+        if (msg.member.voice.channel) {
+            let channel = msg.guild.channels.cache.get(msg.member.voice.channel.id);
+           
+            for (const [memberID, member] of channel.members) {
+              // I added the following if statement to mute everyone but the invoker:
+              // if (member != message.member)
+          
+              // This single line however, nested inside the for loop, should mute everyone in the channel:
+              member.voice.setMute(true).catch(data => console.log(data));
+            }
+          } else {
+            msg.reply('You need to join a voice channel first!');
+          }
+        
+        // mems = msg.member.voice.channel.members;
+        // mems.forEach(x => {
+        //     console.log(x.id)
             
-        });
+        
+    }
+    else if (command == 'tinypenisplease') {
+        msg.channel.send("Here you go...", {
+            files: [
+                "images\\jknop2.PNG"
+            ]
+        })
     }
     else {
         msg.channel.send("Sorry kid it looks like you did something wrong");
